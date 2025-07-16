fe02fa2f95c97b65a483776e517997776f29c8d6
OsirisAlvarez <osiris.alvarez@alumnos.ucn.cl>
2025-07-15 23:24:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-
+{
+    "s1": [
+        ["Introducción a la Adiministración","DAAD00100","7","7","E&A",[],""],
+        ["Introducción a la Economía","DAEC00100","7","7","E&A",[],""],
+        ["Matemática I","DAMA00132","9","9","MATE",[],""],
+        ["Taller de Habilidades","DAAD-00103","3","3","MATE",[],""],
+        ["Nociones de Derecho Civil y Comercial","DAAC00100","4","4","C&T",[],""],
+        ["Formación General Teológica ","UNFG01292","1","1","FG",[],""]
+    ],
+    "s2": [
+        ["Administración","DAAD00202","5","5","E&A",["DAAD00100"],""],
+        ["Macroeconomía","DAEC00200","5","5","E&A",["DAEC00100"],""],
+        ["Matemática II","DAMA00202","6","6","MATE",["DAMA00132"],""],
+        ["Matemática Financiera","DAAD00203","3","3","MATE",[],""],
+        ["Introducción a la Contabilidad","DAAC00201","7","7","C&T",["DAAD00100"],""],
+        ["Nociones de Derecho Laboral","DAAC00202","4","4","C&T",[],""]
+    ],
+    "s3": [
+        ["Miroeconomía","DAEC00301","5","5","E&A",["DAEC00100","DAMA00202"],""],
+        ["Estadística I","DAEC00201","6","6","E&A",["DAMA00202"],""],
+        ["Levantamiento y Modelamiento de Dato","DAAD00301","6","6","E&A",["DAAD00202"],""],
+        ["Contabilidad I","DAAC00301","8","8","C&T",["DAAD00203","DAAC00201","DAAC00202"],""],
+        ["Formación General Globalización I","UNFG01184","4","4","FG",[],""]
+    ],
+    "s4": [
+        ["Recursos Humanos","DAAD00400","5","5","E&A",["DAAD00202"],""],
+        ["Estadística II","DAEC00302","5","5","E&A",["DAEC00201"],""],
+        ["Modelamiento de Procesos y Sistemas","DAAD00401","6","6","E&A",["DAAD00301"],""],
+        ["Contabilidad II","DAAC00402","8","8","C&T",["DAAC00301"],""],
+        ["Formación General Teológica","UNFG02294","2","2","FG",["UNFG01292"],""],
+        ["Formación General Globalización II","UNFG02184","4","4","FG",["UNFG01184"],""]
+    ],
+    "s5": [
+        ["Costos I","DAAC00502","5","5","C&T",["DAAC00301"],""],
+        ["Contabilidad III","DAAC00501","8","8","C&T",["DAAC00402"],""],
+        ["Gestión Tributaria I","DAAC00503","5","5","C&T",["DAAC00402"],""],
+        ["Control Interno","DAAC00504","5","5","AUDIT",["DAAC00402"],""],
+        ["Formación General Teológica","UNFG32943","2","2","FG",["UNFG02294"],""],
+        ["Formación General Globalización III","UNFG03183","3","3","FG",["UNFG02184"],""]
+    ],
+    "s6": [
+        ["Fundamentos de Dirección Estratégica","DAAD00603","5","5","E&A",["DAAD00202"],""],
+        ["Costos II","DAAC00600","5","5","C&T",["DAAC00502"],""],
+        ["Gestión Tributaria II","DAAC00601","6","6","C&T",["DAAC00503"],""],
+        ["Formación General Globalización IV","UNFG04183","3","3","FG",["UNFG03183"],""],
+        ["Integrador I Taller Contable","DAAC00603","11","11","INTRG",["DAAC00502","DAAC00501","DAAC00503","DAAC00504"],""]
+    ],
+    "s7": [
+        ["Control de Gestión","DAAC00702","6","6","E&A",["DAAD00603","DAAC00600"],""],
+        ["Gestión Financiera","DAAD00704","5","5","C&T",["DAAC00501"],""],
+        ["Gestión Tributaria III","DAAC00700","6","6","C&T",["DAAC00503"],""],
+        ["Auditoría de Estados Financieros I","DAAC00602","6","6","AUDIT",["DAAC00504"],""],
+        ["Formación General Teológica","UNFG14299","2","2","FG",["UNFG32943"],""],
+        ["Formación General Electiva","UNFG01148","4","4","FG",[],""],
+        ["Práctica Pre-profesional","AUP01","16","16","INTRG",["DAAC00504","DAAC00603"],""]
+    ],
+    "s8": [
+        ["Planificación de Sistemas de Información","DAAD00802","4","4","E&A",["DAAD00603"],""],
+        ["Finanzas Corporativas","DAAD00803","5","5","C&T",["DAAC00600","DAAD00704"],""],
+        ["Planificación y Auditoría Tributaria","DAAC00802","7","7","C&T",["DAAC00601","DAAC00700"],""],
+        ["Auditoría Estados Financieros II","DAAC00701","6","6","AUDIT",["DAAC00602"],""],
+        ["Auditoría Gestión","DAAC00803","4","4","AUDIT",["DAAC00702","DAAC00504"],""],
+        ["Formación General Electiva","FG2E8","4","4","FG",[],""]
+    ],
+    "s9": [
+        ["Formación Profesional Electiva","UNFP09343","4","4","FPE",[],""],
+        ["Formación Profesional Electiva","UNFP09338","4","4","FPE",[],""],
+        ["Formación Profesional Electiva","UNFP09344","4","4","FPE",[],""],
+        ["Formación Profesional Electiva (Preparación Actividad de Titulación)","UNFP00919","10","10","INTRG",[],""],
+        ["Intregador II Taller Auditoría","DAAC00900","9","9","INTRG",[],""]
+    ],
+    "s10": [
+        ["Formación Profesional Electiva (Actividad de Titulación)","UNFP01047","24","24","INTRG",[],""]
+    ]
+}
